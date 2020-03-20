860fb9e7ba0883b4342e7fb7bb28bf21b8c1082e
Mendonca, Tadeu <tadeu.mendonca@accenture.com>
2020-03-20 16:12:29 -0300

    CityScreen done.

--- clima_flutter_master/lib/screens/location_screen.dart
+++ clima_flutter_master/lib/screens/location_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:clima/services/weather.dart';
+import 'package:clima/screens/city_screen.dart';
 
 class LocationScreen extends StatefulWidget {
   final locationWeather;
@@ -73,7 +74,12 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return CityScreen();
+                      }));
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
